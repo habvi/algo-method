2606ef17ab24f2239684b2125c3852bc619ddb38
habvi <a2231250601h@yahoo.co.jp>
2021-11-24 08:33:08 +0900

    update: indent

--- ナップサックDP編/部分和問題とその応用たち/7.個数制限付き部分和問題.cpp
+++ ナップサックDP編/部分和問題とその応用たち/7.個数制限付き部分和問題.cpp
@@ -7,20 +7,20 @@ int main() {
 	int n, m; cin >> n >> m;
 	vector<int> a(n), b(n);
 	rep(i, n) cin >> a[i] >> b[i];
-
+	
 	int INF = 1001001001;
 	vector<vector<int>> dp(n+1, vector<int>(m+1, INF));
 	dp[0][0] = 0;
 	rep(i, n) rep(j, m+1) {
 		if (dp[i][j] != INF) dp[i+1][j] = 0;
 		if (j-a[i] >= 0) {
-			if (dp[i][j-a[i]] != INF) 
+			if (dp[i][j-a[i]] != INF)
 				dp[i+1][j] = min(dp[i+1][j], 1);
 			if (dp[i+1][j-a[i]] < b[i])
 				dp[i+1][j] = min(dp[i+1][j], dp[i+1][j-a[i]] + 1);
 		}
 	}
-
+	
 	if (dp[n][m] != INF) cout << "Yes" << endl;
 	else cout << "No" << endl;
 }
