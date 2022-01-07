a3c3a6045f2c0e92a58f9eae4e9e975df43d390a
habvi <a2231250601h@yahoo.co.jp>
2022-01-08 00:49:34 +0900

    simpler

--- 13_設計技法とデータ構造/11_データサイエンス入門/697_最頻値.py
+++ 13_設計技法とデータ構造/11_データサイエンス入門/697_最頻値.py
@@ -1,11 +1,23 @@
-from collections import Counter
 n = int(input())
 A = list(map(int, input().split()))
-c = Counter(A)
-mx = c.most_common(1)[0][1]
-ans = []
-for k, v in c.items():
-    if v == mx:
-        ans.append(k)
-ans.sort()
-print("\n".join(map(str, ans)))
+cnt = [0] * (max(A) + 1)
+for a in A:
+    cnt[a] += 1
+mx = max(cnt)
+for i, c in enumerate(cnt):
+    if c == mx:
+        print(i)
+
+
+
+# from collections import Counter
+# n = int(input())
+# A = list(map(int, input().split()))
+# c = Counter(A)
+# mx = c.most_common(1)[0][1]
+# ans = []
+# for k, v in c.items():
+#     if v == mx:
+#         ans.append(k)
+# ans.sort()
+# print("\n".join(map(str, ans)))
