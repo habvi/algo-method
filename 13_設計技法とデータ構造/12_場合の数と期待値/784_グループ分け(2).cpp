cfbe214b2e910fca0c5dfbd283b9794157c7d457
habvi <a2231250601h@yahoo.co.jp>
2022-02-06 16:58:39 +0900

    add: 784

--- 13_設計技法とデータ構造/12_場合の数と期待値/784_グループ分け(2).cpp
+++ 13_設計技法とデータ構造/12_場合の数と期待値/784_グループ分け(2).cpp
@@ -4,8 +4,13 @@ using namespace std;
 #define rep(i, a, b) for (int i = a; i < b; ++i)
 
 int main() {
-	int n, m;
+    int n, m;
     cin >> n >> m;
 
-    
+    int total = n + m - 1;
+    long long ans = 1;
+    rep(i, 0, n) ans *= total - i;
+    rep(i, 0, n) ans /= i + 1;
+
+    cout << ans << endl;
 }
