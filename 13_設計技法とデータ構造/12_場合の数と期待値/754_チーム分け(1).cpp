a3e56d8d9b4ad639aaa60e11c0d800a5fa54e538
habvi <a2231250601h@yahoo.co.jp>
2022-02-06 16:29:52 +0900

    add: 754

---
+++ 13_設計技法とデータ構造/12_場合の数と期待値/754_チーム分け(1).cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define _GLIBCXX_DEBUG
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
+int main() {
+	int n, a, b;
+    cin >> n >> a >> b;
+
+    long long ans = 1;
+    if (a > b) swap(a, b);
+
+    rep(i, 0, a) ans *= n - i;
+    rep(i, 0, a) ans /= i + 1;
+
+    cout << ans << endl;
+}
