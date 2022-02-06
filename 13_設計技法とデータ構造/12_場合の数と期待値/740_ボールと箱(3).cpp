c8801e91ed74e3b09da36377d85c3c3bc2bbebaa
habvi <a2231250601h@yahoo.co.jp>
2022-02-06 16:08:10 +0900

    add: another way

--- 13_設計技法とデータ構造/12_場合の数と期待値/740_ボールと箱(3).cpp
+++ 13_設計技法とデータ構造/12_場合の数と期待値/740_ボールと箱(3).cpp
@@ -2,18 +2,29 @@
 using namespace std;
 #define _GLIBCXX_DEBUG
 #define rep(i, a, b) for (int i = a; i < b; ++i)
+using ll = long long;
 
-int main() {
-    int n, m;
-    cin >> n >> m;
-
-    long long ans = 1;
+// nCm * m!
+long long solve1(int n, int m, ll ans){
     rep(i, 0, m) {
         ans *= n - i;
         ans /= i + 1;
     }
-
     rep(i, 0, m) ans *= m - i;
+    return ans;
+}
 
-    cout << ans << endl;
+// nPm
+long long solve2(int n, int m, ll ans){
+    rep(i, 0, m) ans *= n - i;
+    return ans;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    ll ans = 1;
+
+    cout << solve1(n, m, ans) << endl;
+    // cout << solve2(n, m, ans) << endl;
 }
