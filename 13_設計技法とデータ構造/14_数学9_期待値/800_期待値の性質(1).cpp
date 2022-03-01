90156a76cade9e7ece7754a30bd6441dabf2664d
habvi <a2231250601h@yahoo.co.jp>
2022-03-01 14:27:53 +0900

    add: 800

---
+++ 13_設計技法とデータ構造/14_数学9_期待値/800_期待値の性質(1).cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define _GLIBCXX_DEBUG
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
+int main() {
+    int m, k;
+    cin >> m >> k;
+    // cout << k << endl;
+
+    vector<int> A(m), P(m);
+    rep(i, 0, m) cin >> A[i] >> P[i];
+
+    double Ex = 0.0, Ey = 0.0;
+    rep(i, 0, m) {
+        Ex += A[i] * P[i] / 100.0;
+        Ey += A[i] * k * P[i] / 100.0;
+    }
+
+    cout << fixed << setprecision(10);
+    cout << Ey / Ex << endl;
+}
