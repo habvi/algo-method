52b590a1bd606dc6ca68473a45e98d9e67f11e86
habvi <a2231250601h@yahoo.co.jp>
2022-03-01 13:53:25 +0900

    add: 797

---
+++ 13_設計技法とデータ構造/14_数学9_期待値/797_かたよったくじ(3).cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define _GLIBCXX_DEBUG
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
+int main() {
+    int m;
+    cin >> m;
+    vector<int> A(m), P(m);
+    rep(i, 0, m) cin >> A[i] >> P[i];
+
+    int total = 0;
+    rep(i, 0, m) total += A[i] * P[i];
+
+    double ans = total / 100.0;
+    cout << fixed << setprecision(10);
+    cout << ans << endl;
+}
