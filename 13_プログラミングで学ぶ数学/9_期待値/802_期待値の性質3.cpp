469ae07e5dfbc5a940edab8718862579804a4265
habvi <a2231250601h@yahoo.co.jp>
2022-03-14 19:24:28 +0900

    add: 802

--- 13_プログラミングで学ぶ数学/9_期待値/802_期待値の性質3.cpp
+++ 13_プログラミングで学ぶ数学/9_期待値/802_期待値の性質3.cpp
@@ -4,5 +4,22 @@ using namespace std;
 #define rep(i, a, b) for (int i = a; i < b; ++i)
 
 int main() {
-    
+    int n, m;
+    cin >> n >> m;
+
+    double ans = 0;
+
+    vector<int> A(n), P(n);
+    rep(i, 0, n) {
+        cin >> A[i] >> P[i];
+        ans += A[i] * P[i] / 100.0;
+    }
+
+    vector<int> B(m), Q(m);
+    rep(i, 0, m) {
+        cin >> B[i] >> Q[i];
+        ans += B[i] * Q[i] / 100.0;
+    }
+
+    cout << ans << endl;
 }
