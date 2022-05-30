f68878d1e07854fedb57905542bd9b239f42001f
habvi <a2231250601h@yahoo.co.jp>
2022-05-30 17:32:38 +0900

    add: 759

---
+++ 13_プログラミングで学ぶ数学/8_確率/759_くじ引き_1.rs
@@ -0,0 +1,13 @@
+use std::io::stdin;
+use std::str::FromStr;
+
+fn input_t<T: FromStr>() -> T {
+    let mut s: String = String::new();
+    stdin().read_line(&mut s).ok().unwrap();
+    s.trim().parse().ok().unwrap()
+}
+
+fn main() {
+    let n: f64 = input_t();
+    println!("{}", 1. / n);
+}
