f7489afcffac9ffac7dd338d5661ef2c74365f2a
habvi <a2231250601h@yahoo.co.jp>
2022-07-29 02:12:51 +0900

    another DP

--- 07_DP/3_部分和問題とナップサック問題/342_ナップサック問題.py
+++ 07_DP/3_部分和問題とナップサック問題/342_ナップサック問題.py
@@ -10,3 +10,20 @@ for w, v in zip(W, V):
         if i + w <= M:
             dp[i + w] = max(dp[i + w], dp[i] + v)
 print(max(dp))
+
+
+
+# -------------------------------------
+N, M = map(int, input().split())
+W = list(map(int, input().split()))
+V = list(map(int, input().split()))
+
+INF = float('inf')
+ndp = [-INF] * (M + 1)
+ndp[0] = 0
+for w, v in zip(W, V):
+    dp = ndp[:]
+    for i in range(M + 1):
+        if i + w <= M:
+            ndp[i + w] = max(ndp[i + w], dp[i] + v)
+print(max(ndp))
